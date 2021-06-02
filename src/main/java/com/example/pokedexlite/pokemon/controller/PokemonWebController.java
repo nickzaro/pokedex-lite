ad247226aac3d78ee928a8ae3fcc326ecf5a7b87
nick <nickzaro@gmail.com>
2021-06-02 12:07:56 -0300

    Agregando la vista para pokemon nuevo a base de datos global

--- src/main/java/com/example/pokedexlite/pokemon/controller/PokemonWebController.java
+++ src/main/java/com/example/pokedexlite/pokemon/controller/PokemonWebController.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -36,10 +37,36 @@ public class PokemonWebController {
         Pokemon pokemon = new Pokemon();
         List<Ability> abilities = abilityService.findAll();
         List<Type> types = typeService.findAll();
+        List<Pokemon> pokemons = pokemonService.findAll();
         model.addAttribute("title","New Pokemon");
-        model.addAttribute("abilities", abilities);
-        model.addAttribute("types",types);
+        model.addAttribute("allAbilities", abilities);
+        model.addAttribute("allTypes",types);
+        model.addAttribute("allPokemons",pokemons);
         model.addAttribute("pokemon",pokemon);
+        return "pokemon/addPokemon";
+    }
+
+    @PostMapping("/add")
+    public String saveNewPokemon(Pokemon pokemon){
+        System.out.println("ID: " + pokemon.getPokemonId());
+        System.out.println("Nombre: " + pokemon.getName());
+        System.out.println("Nivel: " + pokemon.getLevel());
+        System.out.println("Evolución ID: " + pokemon.getPokemonEvolution());
+
+        System.out.println("cantidad de tipos: " + pokemon.getTypes().size());
+        for( Type type: pokemon.getTypes()){
+            System.out.println(type.getId());
+            System.out.println(type.getName());
+            System.out.println(type.getDescription());
+        }
+        System.out.println("cantidad de habilidades: " + pokemon.getTypes().size());
+        for( Ability ability: pokemon.getAbilities()){
+            System.out.println(ability.getId());
+            System.out.println(ability.getName());
+            System.out.println(ability.getDescription());
+        }
+        pokemonService.save(pokemon);
+
         return "pokemon/addPokemon";
     }
 }
