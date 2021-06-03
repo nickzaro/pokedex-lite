7d1256a9f73ccaab106f5aa8b90cded695c62992
Nick Zárate <nickzaro@gmail.com>
2021-06-03 11:07:52 -0300

    Agregando BaseController para redireccionar desde la dirección base a user/

---
+++ src/main/java/com/example/pokedexlite/user/controller/BaseController.java
@@ -0,0 +1,15 @@
+package com.example.pokedexlite.user.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class BaseController {
+
+    @GetMapping({"/", ""})
+    public String redireccionando() {
+        return "redirect:user/";
+    }
+}
